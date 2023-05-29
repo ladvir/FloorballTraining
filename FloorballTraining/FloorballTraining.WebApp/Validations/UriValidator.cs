ad536ae8c8dbc685349836acdf6260f3cbeca0b7
viragla <ladislav.virag@storaenso.com>
2023-05-29 10:38:46 +0200

    linky na aktivite

--- FloorballTraining/FloorballTraining.WebApp/Validations/UriValidator.cs
+++ FloorballTraining/FloorballTraining.WebApp/Validations/UriValidator.cs
@@ -6,7 +6,7 @@ namespace FloorballTraining.WebApp.Validations
     {
         public UriValidator()
         {
-            RuleFor(x => x).Must(x => x != null && Uri.IsWellFormedUriString(x, UriKind.Absolute)).WithMessage("Neplatný odkaz");
+            RuleFor(x => x).Must(x => x != null && Uri.IsWellFormedUriString(x, UriKind.Absolute)).WithMessage("Neplatný odkaz. Zadej adresu včetně https://");
 
 
         }
