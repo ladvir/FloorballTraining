300913fc234d76bdc1edd4f56077a475e65e2ebb
viragla <ladislav.virag@storaenso.com>
2023-07-21 09:49:52 +0200

    drawing

--- FloorballTraining/FloorballTraining.WebApp/wwwroot/DrawingTool.js
+++ FloorballTraining/FloorballTraining.WebApp/wwwroot/DrawingTool.js
@@ -3,8 +3,12 @@ let container;
 let isDrawing = false;
 let tool = "";
 let layer ;
+let selectedShape = null;
+let selectedShapeOriginal = null;
 let mouseDownPosition = null;
 
+let backgroundLayer ;
+let backgroundRect;
 const toolColorPicker = document.getElementById('colorpicker');
 
 var stage;
@@ -12,23 +16,93 @@ var stage;
 export function init(containerId) {
     container = document.getElementById(containerId);
 
-    
+
     stage = new window.Konva.Stage({
-    container: containerId,
-    width: window.innerWidth - container.offsetLeft,
-    height: window.innerHeight - container.offsetTop
-});
+        container: containerId,
+        width: window.innerWidth - container.offsetLeft,
+        height: window.innerHeight - container.offsetTop
+    });
 
-layer = new window.Konva.Layer();
+    layer = new window.Konva.Layer();
+    backgroundLayer = new window.Konva.Layer();
 
-stage.add(layer);
-stage.draw();
+    stage.add(backgroundLayer);
 
-stage.on("mousedown", mouseDownHandler);
-stage.on("mousemove", mouseMoveHandler);
-stage.on("mouseup", mouseUpHandler);
+    stage.add(layer);
+    stage.draw();
+
+    stage.on("mousedown", mouseDownHandler);
+    stage.on("mousemove", mouseMoveHandler);
+    stage.on("mouseup", mouseUpHandler);
+
+    window.addEventListener('resize', function() {
+        updateStageSize();
+    });
+
+loadImages(sources, function(images) {
+            drawBackGround(images);
+        });
+
+
 }
 
+
+function drawPlayer() {
+    isDrawing = false;
+    createImageElement(sources.player);
+    return null;
+}
+
+function createImageElement(src) {
+
+    let imageObj = new Image();
+
+    
+    imageObj.src = src;
+
+
+    const image = new window.Konva.Image({
+        image: imageObj,
+        x: stage.getPointerPosition().x,
+        y: stage.getPointerPosition().y,
+        draggable: true
+    });
+
+    layer.add(image);
+    layer.draw();
+
+    return image;
+}
+
+
+function drawBackGround(images) {
+   backgroundRect = new window.Konva.Image({
+       x: 0,
+       y: 0,
+       width: window.innerWidth - container.offsetLeft,
+       height: window.innerHeight - container.offsetTop,
+       
+       image : images.florballsvg
+   });
+
+   
+
+
+
+// Add the background rectangle to the background layer
+    backgroundLayer.add(backgroundRect);
+
+// Draw the stage
+    backgroundLayer.draw();
+}
+
+function updateStageSize() {
+    stage.width(window.innerWidth - container.offsetLeft);
+    stage.height(window.innerHeight - container.offsetTop);
+    backgroundRect.width(window.innerWidth - container.offsetLeft);
+    backgroundRect.height(window.innerHeight - container.offsetTop);
+    backgroundLayer.batchDraw();
+}
 
 export function setTool(toolid) {
     tool = toolid;
@@ -51,6 +125,8 @@ function clearStage() {
     layer.draw();
 }
 
+
+
 function getMousePosition(event, element) {
     let position = {
         x: event.clientX - element.offsetLeft,
@@ -68,11 +144,19 @@ function mouseDownHandler() {
         case "gate": shape = drawGate();  break;
         case "cone": shape = drawCone(); break;
         case "ball": shape = drawBall(); break;
+        
+            case "delete":
+                deleteSelectedShape();
+                break;
+
     }
 
     if (shape !== null) {
         layer.add(shape).batchDraw();
     }
+
+
+
 }
 
 function mouseMoveHandler() {
@@ -84,27 +168,18 @@ function mouseUpHandler() {
     return true;
 }
 
-function drawPlayer() {
-    isDrawing = false;
-    
-    return new window.Konva.Rect({
-        x: stage.getPointerPosition().x,
-        y: stage.getPointerPosition().y,
-        width: 20,
-        height: 20,
-        stroke: toolColorPicker.value,
-        draggable: true
-    });
-}
+
 function drawGate() {
     isDrawing = false;
-    return new window.Konva.Rect({
+
+    return new window.Konva.Path({
         x: stage.getPointerPosition().x,
         y: stage.getPointerPosition().y,
-        width: 200,
-        height: 200,
-        stroke: toolColorPicker.value,
-        draggable: true
+        data:
+            'M213.1,6.7c-32.4-14.4-73.7,0-88.1,30.6C110.6,4.9,67.5-9.5,36.9,6.7C2.8,22.9-13.4,62.4,13.5,110.9C33.3,145.1,67.5,170.3,125,217c59.3-46.7,93.5-71.9,111.5-106.1C263.4,64.2,247.2,22.9,213.1,6.7z',
+        fill: 'green',
+        scaleX: 0.5,
+        scaleY: 0.5,
     });
 }
 
@@ -145,4 +220,33 @@ function downloadUri(uri, name) {
     link = null;
     //delete link;
 }
+
+function loadImages(sources, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+    // get num of sources
+    for (var src in sources) {
+        numImages++;
+    }
+    for (var src in sources) {
+        images[src] = new Image();
+        images[src].onload = function () {
+            if (++loadedImages >= numImages) {
+                callback(images);
+            }
+        };
+        images[src].src = sources[src];
+    }
+}
+
+var sources = {
+    florball: '/assets/florball.png',
+    florballsvg: '/assets/florball.svg',
+    hriste: '/assets/hriste.png',
+    player: '/assets/man.svg'
+
+};
+
+
 
