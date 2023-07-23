ebc19c3b2e2a7efd0dbd9d43afeacf39f9c48d99
viragla <ladislav.virag@storaenso.com>
2023-07-24 00:52:45 +0200

    vymena pozadi 1

--- FloorballTraining/FloorballTraining.WebApp/wwwroot/DrawingTool.js
+++ FloorballTraining/FloorballTraining.WebApp/wwwroot/DrawingTool.js
@@ -2,13 +2,10 @@
 let container;
 let isDrawing = false;
 let tool = "";
-let layer ;
+let layer;
 let transformer;
-let selectedShape = null;
-let selectedShapeOriginal = null;
-let mouseDownPosition = null;
 
-let backgroundLayer ;
+let backgroundLayer;
 let backgroundRect;
 const toolColorPicker = document.getElementById('colorpicker');
 
@@ -17,191 +14,26 @@ var stage;
 let images;
 var x1, y1, x2, y2;
 
-export function init(containerId) {
-    container = document.getElementById(containerId);
+
+var sources = {
+    blankIco: '/assets/fields/blank.png',
+    blankSvg: '/assets/fields/blank.svg',
+    emptyIco: '/assets/fields/field.png',
+    emptySvg: '/assets/fields/field.svg',
+
+    hriste: '/assets/hriste.png',
+    player: '/assets/man.svg',
+};
 
 
-    stage = new window.Konva.Stage({
-        container: containerId,
-        width: window.innerWidth - container.offsetLeft,
-        height: window.innerHeight - container.offsetTop
-    });
-    backgroundLayer = new window.Konva.Layer();
-
-    stage.add(backgroundLayer);
-
-
-    layer = new window.Konva.Layer();
-    stage.add(layer);
-
-    transformer = new window.Konva.Transformer();
-    layer.add(transformer);
-
-    // add a new feature, lets add ability to draw selection rectangle
-    var selectionRectangle = new window.Konva.Rect({
-        fill: 'rgba(0,0,255,0.5)',
-        visible: false
-    });
-    layer.add(selectionRectangle);
-
-
-
-    
-
-    stage.on('mousedown touchstart', (e) => {
-        // do nothing if we mousedown on any shape
-        if (e.target !== stage) {
-          return;
-        }
-
-        if (tool === "") {
-            e.evt.preventDefault();
-            x1 = stage.getPointerPosition().x;
-            y1 = stage.getPointerPosition().y;
-            x2 = stage.getPointerPosition().x;
-            y2 = stage.getPointerPosition().y;
-
-            selectionRectangle.visible(true);
-            selectionRectangle.width(0);
-            selectionRectangle.height(0);
-        } else {
-
-            isDrawing = true;
-
-            var shape = null;
-            switch (tool) {
-            case "player":
-                shape = drawPlayer();
-                break;
-            case "gate":
-                shape = drawGate();
-                break;
-            case "cone":
-                shape = drawCone();
-                break;
-            case "ball":
-                shape = drawBall();
-                break;
-
-            case "delete":
-                deleteSelectedShape();
-                break;
-
-            }
-
-            if (shape !== null) {
-                layer.add(shape).batchDraw();
-            }
-        }
-
-    });
-
-      stage.on('mousemove touchmove', (e) => {
-          
-
-        // do nothing if we didn't start selection
-        if (!selectionRectangle.visible()) {
-          return;
-        }
-        e.evt.preventDefault();
-        x2 = stage.getPointerPosition().x;
-        y2 = stage.getPointerPosition().y;
-
-        selectionRectangle.setAttrs({
-          x: Math.min(x1, x2),
-          y: Math.min(y1, y2),
-          width: Math.abs(x2 - x1),
-          height: Math.abs(y2 - y1),
-        });
-      });
-
-      stage.on('mouseup touchend', (e) => {
-
-         
-        // do nothing if we didn't start selection
-        if (!selectionRectangle.visible() ) {
-          return false;
-        }
-        e.evt.preventDefault();
-        // update visibility in timeout, so we can check it in click event
-        setTimeout(() => {
-          selectionRectangle.visible(false);
-        });
-
-        var shapes = stage.find('.mydrawing');
-        var box = selectionRectangle.getClientRect();
-        var selected = shapes.filter((shape) =>
-          window.Konva.Util.haveIntersection(box, shape.getClientRect())
-        );
-        transformer.nodes(selected);
-      });
-
-      // clicks should select/deselect shapes
-      stage.on('click tap', function (e) {
-        // if we are selecting with rect, do nothing
-        if (selectionRectangle.visible()) {
-          return;
-        }
-
-        // if click on empty area - remove all selections
-        if (e.target === stage) {
-            transformer.nodes([]);
-          return;
-        }
-
-        // do nothing if clicked NOT on our rectangles
-        if (!e.target.hasName('mydrawing')) {
-          return;
-        }
-
-        // do we pressed shift or ctrl?
-        const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
-        const isSelected = transformer.nodes().indexOf(e.target) >= 0;
-
-        if (!metaPressed && !isSelected) {
-          // if no key pressed and the node is not selected
-          // select just one
-          transformer.nodes([e.target]);
-        } else if (metaPressed && isSelected) {
-          // if we pressed keys and node was selected
-          // we need to remove it from selection:
-          const nodes = transformer.nodes().slice(); // use slice to have new copy of array
-          // remove node from array
-          nodes.splice(nodes.indexOf(e.target), 1);
-          transformer.nodes(nodes);
-        } else if (metaPressed && !isSelected) {
-          // add the node into selection
-          const nodes = transformer.nodes().concat([e.target]);
-          transformer.nodes(nodes);
-        }
-      });
-
-    stage.draw();
-
-    
-
-    loadImages(sources, function(locimages) {
-        images = locimages;
-        //drawBackGround(images);
-    });
-
-
-}
-
-function deleteSelectedShape() {
-    //transformer.
-}
 
 function drawPlayer() {
     isDrawing = false;
     createImageElement(images.player);
-    
+
 }
 
 function createImageElement(src) {
-
-    
-
     const image = new window.Konva.Image({
         image: src,
         x: stage.getPointerPosition().x,
@@ -216,30 +48,57 @@ function createImageElement(src) {
     return image;
 }
 
+function clearBackgroundLayer() {
+    backgroundLayer.removeChildren(); // Remove all the child nodes from the background layer
+    backgroundLayer.draw(); // Redraw the layer to update the changes
+}
+function drawBackGround(src) {
+    
 
-function drawBackGround(images) {
-   backgroundRect = new window.Konva.Image({
-       x: 0,
-       y: 0,
-       image : images.florballsvg
-   });
+    clearBackgroundLayer();
 
-   backgroundLayer.add(backgroundRect);
-   backgroundLayer.draw();
+    backgroundRect = new window.Konva.Image({
+        x: 0,
+        y: 0,
+        image: src,
+        name: 'backgroundrect'
+    });
+
+    backgroundLayer.add(backgroundRect);
+    backgroundLayer.draw();
+    resizeBackgroundLayer();
 }
 
-export function setTool(toolid) {
-    tool = toolid;
+function resizeBackgroundLayer() {
+    var newWidth = window.innerWidth - container.offsetLeft;
+    var newHeight = window.innerWidth - container.offsetLeft;
+
+    // Calculate the aspect ratio of the original image
+    var imageAspectRatio = backgroundLayer.width() / backgroundLayer.height();
+
+    // Calculate the aspect ratio of the new dimensions
+    var newAspectRatio = newWidth / newHeight;
+
+    if (newAspectRatio > imageAspectRatio) {
+        // Fit the width to the container and adjust the height accordingly
+        stage.width(newWidth);
+        stage.height(newWidth / imageAspectRatio);
+    } else {
+        // Fit the height to the container and adjust the width accordingly
+        stage.height(newHeight);
+        stage.width(newHeight * imageAspectRatio);
+    }
+
+    // Center the image on the stage
+    backgroundLayer.position({
+        x: 0,
+        y: 0
+    });
+
+    stage.draw();
 }
 
-export function newDrawing() {
-    clearStage();
-}
 
-export function saveDrawing() {
-    var dataUrl = stage.toDataURL({ pixelRatio: 1 });
-        downloadUri(dataUrl, 'stage.png');
-}
 
 
 function clearStage() {
@@ -250,23 +109,13 @@ function clearStage() {
 }
 
 
-
-function getMousePosition(event, element) {
-    let position = {
-        x: event.clientX - element.offsetLeft,
-        y: event.clientY - element.offsetTop
-    };
-    return position;
-}
-
 function drawGate() {
     isDrawing = false;
 
     return new window.Konva.Path({
         x: stage.getPointerPosition().x,
         y: stage.getPointerPosition().y,
-        data:
-            'M213.1,6.7c-32.4-14.4-73.7,0-88.1,30.6C110.6,4.9,67.5-9.5,36.9,6.7C2.8,22.9-13.4,62.4,13.5,110.9C33.3,145.1,67.5,170.3,125,217c59.3-46.7,93.5-71.9,111.5-106.1C263.4,64.2,247.2,22.9,213.1,6.7z',
+        data: '"<svg style=""width: 24px; height: 24px; "" viewBox=""0 0 24 24""><circle cx=""32"" cy=""13.48"" r=""5.91"" stroke-linecap=""round""/><path d=""M25.48,56.43V43.83a2.18,2.18,0,0,0-.73-1.64l-2.19-2a1.11,1.11,0,0,1-.36-.82v-13a2.21,2.21,0,0,1,2.2-2.21H38.5a3.31,3.31,0,0,1,3.3,3.31v12a1.14,1.14,0,0,1-.3.76l-2.11,2.25a2.18,2.18,0,0,0-.6,1.51V56.43"" /></svg>"',
         fill: 'green',
         scaleX: 0.5,
         scaleY: 0.5,
@@ -287,14 +136,14 @@ function drawCone() {
 }
 
 function drawBall() {
-    
+
     isDrawing = false;
     return new window.Konva.Circle({
 
         x: stage.getPointerPosition().x,
         y: stage.getPointerPosition().y,
-        radius : 50,
-        stroke : toolColorPicker.value,
+        radius: 50,
+        stroke: toolColorPicker.value,
         draggable: true,
         name: 'mydrawing'
     });
@@ -308,7 +157,7 @@ function downloadUri(uri, name) {
     link.href = uri;
     document.body.appendChild(link);
     link.click();
-    
+
     document.body.removeChild(link);
     link = null;
     //delete link;
@@ -318,8 +167,8 @@ function downloadUri(uri, name) {
 function countProperties(obj) {
     var count = 0;
 
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop))
             ++count;
     }
 
@@ -328,11 +177,11 @@ function countProperties(obj) {
 function loadImages(sources, callback) {
     var images = {};
     var loadedImages = 0;
-    var numImages  = countProperties(sources);
+    var numImages = countProperties(sources);
 
     for (var src in sources) {
-        if(sources.hasOwnProperty(src))
-        images[src] = new Image();
+        if (sources.hasOwnProperty(src))
+            images[src] = new Image();
         images[src].onload = function () {
             if (++loadedImages >= numImages) {
                 callback(images);
@@ -342,13 +191,228 @@ function loadImages(sources, callback) {
     }
 }
 
-var sources = {
-    florball: '/assets/florball.png',
-    florballsvg: '/assets/florball.svg',
-    hriste: '/assets/hriste.png',
-    player: '/assets/man.svg'
 
-};
+function replaceString(oldS, newS, fullS) {
+    return fullS.split(oldS).join(newS);
+}
 
 
+export function setTool(toolid) {
+    tool = toolid;
+}
 
+export function setField(field) {
+
+    field = replaceString(".png", ".svg", field);
+    for (var img in images) {
+        if (images.hasOwnProperty(img))
+            if (images[img].src.endsWith(field)) {
+                drawBackGround(images[img]);
+                break;
+            }
+    }
+}
+
+export function newDrawing() {
+    clearStage();
+}
+
+export function saveDrawing() {
+    var dataUrl = stage.toDataURL({ pixelRatio: 1 });
+    downloadUri(dataUrl, 'stage.png');
+}
+
+
+export function deleteSelectedShapes() {
+    
+    transformer.nodes().forEach(node => {
+        if (node instanceof window.Konva.Shape) {
+            
+                node.destroy(); // Remove the selected shape from the layer
+                transformer.detach(); // Detach the transformer from the shape
+                layer.draw(); // Redraw the layer without the deleted shape
+            
+        }
+    });
+}
+
+
+export function init(containerId) {
+    container = document.getElementById(containerId);
+
+
+    stage = new window.Konva.Stage({
+        container: containerId,
+        width: window.innerWidth - container.offsetLeft,
+        height: window.innerHeight - container.offsetTop
+    });
+    backgroundLayer = new window.Konva.Layer();
+    backgroundLayer.name('backgroundlayer');
+
+    stage.add(backgroundLayer);
+
+
+    layer = new window.Konva.Layer();
+    stage.add(layer);
+
+    transformer = new window.Konva.Transformer();
+    layer.add(transformer);
+
+    // add a new feature, lets add ability to draw selection rectangle
+    var selectionRectangle = new window.Konva.Rect({
+        fill: 'rgba(0,0,255,0.5)',
+        visible: false
+    });
+    layer.add(selectionRectangle);
+
+    stage.on('mousedown touchstart', (e) => {
+
+        if (e.target !== stage && e.target !== backgroundRect) {
+            return;
+        }
+
+        if (tool === "") {
+            e.evt.preventDefault();
+            x1 = stage.getPointerPosition().x;
+            y1 = stage.getPointerPosition().y;
+            x2 = stage.getPointerPosition().x;
+            y2 = stage.getPointerPosition().y;
+
+            selectionRectangle.visible(true);
+            selectionRectangle.width(0);
+            selectionRectangle.height(0);
+        } else {
+
+            isDrawing = true;
+
+            var shape = null;
+            switch (tool) {
+                case "player":
+                    shape = drawPlayer();
+                    break;
+                case "gate":
+                    shape = drawGate();
+                    break;
+                case "cone":
+                    shape = drawCone();
+                    break;
+                case "ball":
+                    shape = drawBall();
+                    break;
+
+            }
+
+            //if (shape !== null) {
+            //    layer.add(shape).batchDraw();
+            //}
+        }
+
+    });
+
+    stage.on('mousemove touchmove', (e) => {
+
+
+        // do nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+            return;
+        }
+        e.evt.preventDefault();
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
+
+        selectionRectangle.setAttrs({
+            x: Math.min(x1, x2),
+            y: Math.min(y1, y2),
+            width: Math.abs(x2 - x1),
+            height: Math.abs(y2 - y1)
+        });
+    });
+
+    stage.on('mouseup touchend', (e) => {
+
+
+        // do nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+            return false;
+        }
+        e.evt.preventDefault();
+        // update visibility in timeout, so we can check it in click event
+        setTimeout(() => {
+            selectionRectangle.visible(false);
+        });
+
+        var shapes = stage.find('.mydrawing');
+        var box = selectionRectangle.getClientRect();
+        var selected = shapes.filter((shape) =>
+            window.Konva.Util.haveIntersection(box, shape.getClientRect())
+        );
+        transformer.nodes(selected);
+        return null;
+    });
+
+    window.addEventListener('resize', function () {
+        // Throttle the event to avoid excessive resizing calls
+        clearTimeout(window.resizeTimer);
+        window.resizeTimer = setTimeout(function () {
+            resizeBackgroundLayer();
+        }, 200);
+    });
+
+    // clicks should select/deselect shapes
+    stage.on('click tap', function (e) {
+        // if we are selecting with rect, do nothing
+        if (selectionRectangle.visible()) {
+            return;
+        }
+
+        // if click on empty area - remove all selections
+        if (e.target === stage) {
+            transformer.nodes([]);
+
+            return;
+        }
+
+        // do nothing if clicked NOT on our rectangles
+        if (!e.target.hasName('mydrawing')) {
+            return;
+        }
+
+        // do we pressed shift or ctrl?
+        const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+        const isSelected = transformer.nodes().indexOf(e.target) >= 0;
+
+        if (!metaPressed && !isSelected) {
+            // if no key pressed and the node is not selected
+            // select just one
+            transformer.nodes([e.target]);
+
+      
+
+        } else if (metaPressed && isSelected) {
+            // if we pressed keys and node was selected
+            // we need to remove it from selection:
+            
+            const nodes = transformer.nodes().slice(); // use slice to have new copy of array
+            // remove node from array
+            nodes.splice(nodes.indexOf(e.target), 1);
+            
+            transformer.nodes(nodes);
+        } else if (metaPressed && !isSelected) {
+            // add the node into selection
+            const nodes = transformer.nodes().concat([e.target]);
+            
+            transformer.nodes(nodes);
+        }
+    });
+
+    stage.draw();
+
+
+
+    loadImages(sources, function (locimages) {
+        images = locimages;
+        drawBackGround(images.emptySvg);
+    });
+
+
+}
