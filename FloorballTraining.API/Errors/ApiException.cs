f0bc09aba454d2271d5a76fe8fef3d8f9177922a
viragla <ladislav.virag@storaenso.com>
2025-10-01 15:26:34 +0200

    Opraveno: EditForm na stránce editace sezóny

--- FloorballTraining.API/Errors/ApiException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FloorballTraining.API.Errors;
-
-public class ApiException(int statusCode, string? message = null, string? details = null)
-    : ApiResponse(statusCode, message)
-{
-    public string? Details { get; set; } = details;
-}
--- FloorballTraining.API/Errors/ApiValidationErrorResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FloorballTraining.API.Errors;
-
-public class ApiValidationErrorResponse() : ApiResponse(400)
-{
-    public IEnumerable<string> Errors { get; set; } = new List<string>();
-}
