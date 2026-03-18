a99049b3898cb1d58a9461add535ea2726a4311f
viragla <ladislav.virag@storaenso.com>
2026-03-18 21:43:56 +0100

    Update Vite configuration base path and simplify exception handling response

--- FloorballTraining.API/Middlewares/ExceptionMiddleware.cs
+++ FloorballTraining.API/Middlewares/ExceptionMiddleware.cs
@@ -21,9 +21,7 @@ namespace FloorballTraining.API.Middlewares
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                var response = environment.IsDevelopment()
-                    ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace)
-                    : new ApiException((int)HttpStatusCode.InternalServerError);
+                var response = new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace);
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
